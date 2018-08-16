84171b21811386e45388b698af0502de4baf3145
Darío Macchi <dariomac@gmail.com>
2018-08-15 22:55:40 -0300

    Add autodeployer to the project

--- server.js
+++ server.js
@@ -1,25 +1,72 @@
-var express = require('express'),
-    fs = require('fs');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const exec = require('child_process').exec;
+const async = require('async');
 
-var publicdir = __dirname + '/www';
+var publicdir = path.join(__dirname, '/www');
 
 var app = express();
 
-//http://stackoverflow.com/a/16895480/126519
-app.use(function(req, res, next) {
+// http://stackoverflow.com/a/16895480/126519
+app.use(function (req, res, next) {
   if (req.path.indexOf('.') === -1) {
     var file = publicdir + req.path + '.html';
-    fs.exists(file, function(exists) {
-      if (exists)
+    fs.exists(file, function (exists) {
+      if (exists) {
         req.url += '.html';
+      }
       next();
     });
   }
-  else
+  else {
     next();
+  }
 });
 app.use(express.static(publicdir));
 
 app.listen(3000, function () {
-    console.log('\r\nDariomac.com server listening on: 3000');
+  console.log('\r\nDariomac.com server listening on: 3000');
 });
+
+const absolutePath = __dirname;
+
+const cmds = ['git pull'].concat(process.argv.filter((arg, index) => { return index > 2; }));
+
+const execCmds = cmds.map((cmd) => {
+  return function (callback) {
+    exec(`cd ${absolutePath} && ${cmd}`, {maxBuffer: 1024 * 600}, (err, stdout, stderr) => {
+      if (err) return callback(err);
+      callback(null, `--- ${cmd} ---:\n stdout: ${stdout} \n stderr: ${stderr}\n`);
+    });
+  };
+});
+
+const updateProject = function (callback) {
+  async.series(execCmds, function (err, results) {
+    if (err) return callback(err);
+    return callback(null, results.join(''));
+  });
+};
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('An event has been detected on the listened port: starting execution...');
+
+  updateProject((e, result) => {
+    let response = '';
+    if (e) {
+      console.error(`exec error: ${e}`);
+      response += `exec error: ${e}`;
+    }
+    if (result) {
+      console.log(result);
+      response += `\n ${result}`;
+    }
+    res.end(response);
+  });
+
+}).listen(1337);
+
+console.log('Git-auto-pull is running');
