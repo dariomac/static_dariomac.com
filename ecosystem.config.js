5835470afb4b4a94814d6e6b76680e63e1529d50
Darío Macchi <dariomac@gmail.com>
2018-08-19 22:35:41 -0300

    Add dev deploy configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -34,12 +34,12 @@ module.exports = {
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'dariomac',
+      host : 'https://bitbucket.org',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      repo : 'dariomac@bitbucket.org/dariomac/static_dariomac.com.git',
+      path : '.',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env dev',
       env  : {
         NODE_ENV: 'dev'
       }
