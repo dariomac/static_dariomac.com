2f5e71a5ea6fdec78d8757927d1c9abd677d5137
dariomac <dariomac@gmail.com>
2019-11-11 22:34:53 -0300

    Remove .map files and fix some small issues

---
+++ www/assets/build/precache-manifest.bc82ecd751a2c08675ecb431b9e6801f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b4f31de48535d7a8808d0cf36e0f12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753e2a0bedcc39135bb4",
+    "url": "/static/css/main.004975f5.chunk.css"
+  },
+  {
+    "revision": "72811164706917f40f23",
+    "url": "/static/js/2.c45e0890.chunk.js"
+  },
+  {
+    "revision": "753e2a0bedcc39135bb4",
+    "url": "/static/js/main.6a3a8f4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
