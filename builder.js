4c9a17192135dc959473877fab89ec160a6e0f35
Gabriel Rodriguez <dariomac@gmail.com>
2020-08-07 17:38:53 -0300

    Toward my own Zettelkasten: first step -> Linking and Backlinking capability

--- builder.js
+++ builder.js
@@ -1,4 +1,34 @@
 const toS = require('./lib/2static');
+const toZ = require('./lib/2zettelkasten');
+const cmdArgs = require('command-line-args');
 
-toS.initialize();
-toS.run();
+const settings = [
+  { name: 'paths', multiple: true, defaultOption: true },
+  { name: 'clear-all', alias: 'c', type: Boolean },
+  { name: 'verbose', alias: 'v', type: Boolean }];
+
+const mainOptions = cmdArgs(settings, { stopAtFirstUnknown: true });
+    
+if (!mainOptions.paths || mainOptions.paths.length !== 3) {
+  throw new Error('Invalid number of parameters: you should provide input, output and layout paths as first 3 parameters.');
+}
+
+const paths = {
+  'inputPath': `./${ensureSlashTermination(mainOptions.paths[0])}`,
+  'outputPath': `./${ensureSlashTermination(mainOptions.paths[1])}`,
+  'layoutPath': `./${ensureSlashTermination(mainOptions.paths[2])}`,
+  'assetsPath': `./${ensureSlashTermination(mainOptions.paths[1])}assets/`
+};
+
+function ensureSlashTermination (somePath) {
+  return (somePath.lastIndexOf('/') === somePath.length - 1) ? somePath : `${somePath}/`;
+}
+
+(async function () {
+
+  const backlinks = await toZ.buildBacklinks(mainOptions, paths);
+console.log(backlinks);
+  toS.initialize(mainOptions, paths, backlinks);
+  toS.run();
+
+})();
