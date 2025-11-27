16feaf53edbc93222f2ec55dc985182d8766359c
Dario Macchi <dariomac@gmail.com>
2025-11-27 10:01:03 -0300

    chore: cleanup of old ssl generation approach

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,33 +18,5 @@ module.exports = {
         NODE_ENV: 'production'
       }
     }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-  
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.cjs --env production'
-    },
-    dev : {
-      key  : '/home/dmacchi/.ssh/id_rsa_dm.com.pub',
-      user : 'dmacchi',
-      host : 'bitbucket.org',
-      ref  : 'origin/master',
-      repo : 'git@bitbucket.org:dariomac/static_dariomac.com.git',
-      path : '.',
-      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.cjs --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
-  */
+  ]
 }
